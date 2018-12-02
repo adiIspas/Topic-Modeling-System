8c540db2b37559fedf356afba4d07710c840f633
Adrian Ispas <adrian.ispas@ab4.systems>
2018-12-02 12:14:08 +0200

    Remove unused code and files

--- src/processing/docs_processor.py
+++ src/processing/docs_processor.py
@@ -1,7 +1,6 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import numpy as np
-from array import array
+
 
 class Processor(object):
 
@@ -11,10 +10,6 @@ class Processor(object):
 
         self.word_frequency = 0
         self.stopwords = set(stopwords.words(stopwords_language))
-
-    @staticmethod
-    def generate_data_temp(number_of_documents, number_of_words):
-        return np.random.randint(0, 10, size=(number_of_documents, number_of_words))
 
     def create_vocabulary(self, documents):
         vocabulary = dict()
@@ -27,20 +22,14 @@ class Processor(object):
                         vocabulary[word] = 0
                     vocabulary[word] += 1
 
-        # sorted_vocabulary = filter(lambda x: x[1] > self.word_frequency, sorted(vocabulary.items(), key=lambda x: x[1], reverse=True))
-        sorted_vocabulary = filter(lambda x: x[1] > self.word_frequency, Processor.do_nothing(vocabulary.items()))
-        # sorted_vocabulary_2 = filter(lambda x: x[1] > self.word_frequency, sorted(vocabulary.items(), key=lambda x: x[1], reverse=True))
-        sorted_vocabulary_2 = filter(lambda x: x[1] > self.word_frequency, Processor.do_nothing(vocabulary.items()))
+        sorted_vocabulary = filter(lambda x: x[1] > self.word_frequency, sorted(vocabulary.items(), key=lambda x: x[1], reverse=True))
+        sorted_vocabulary_2 = filter(lambda x: x[1] > self.word_frequency, sorted(vocabulary.items(), key=lambda x: x[1], reverse=True))
 
         word_id = {v[0]: i for i, v in enumerate(sorted_vocabulary)}
         id_word = {i: v[0] for i, v in enumerate(sorted_vocabulary_2)}
         vocabulary_size = len(word_id)
 
         return sorted_vocabulary, word_id, id_word, vocabulary_size
-
-    @staticmethod
-    def do_nothing(my_list):
-        return my_list
 
     @staticmethod
     def generate_data_from_documents(documents, word_ids):
